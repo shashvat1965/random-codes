035437c3909ef293a90b37cd3ed691e213d129ba
shashvat1965 <shashvat1965@gmail.com>
2022-04-17 20:28:38 +0530

    ok ok

--- pointers.c
+++ pointers.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-
-int a=5;
-int * ptr;
-ptr = &a;
-int * q = &ptr;
-printf("%p\n", ptr);
-printf("%p\n", *q);
-printf("%p\n", a);
-printf("%d\n", a);
-
+void swap(int *x, int *y) {
+int temp = *x;
+*x = *y;
+*y = temp;
 }
+
+int main(){
+int a = 10, b = 20;
+printf("Before Swapping %d %d\n", a, b);
+swap(&a, &b);
+printf("After Swapping %d %d\n", a, b);
+return 0;
+}
+
