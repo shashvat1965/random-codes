739f90798e19300d02ffe0dc1470f4511e8aeb72
shashvat <shashvat1965@gmail.com>
2022-05-27 15:21:53 +0530

    ok

--- lol.py
+++ lol.py
@@ -1,2 +1,9 @@
-x=int(input())
-print(2*x)					
+#write a code to take one number as input and print its factorial
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print(factorial(int(input("Enter a number: "))))
