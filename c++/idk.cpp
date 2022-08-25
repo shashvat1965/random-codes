a924ebe896d80bce6072e9b3200d40aac2128975
shashvat1965 <shashvat1965@gmail.com>
2022-08-26 03:35:23 +0530

    idk

---
+++ c++/idk.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t-->0){
+	    int w,x,y,z;
+	    cin >> w;
+		cin >> x;
+		cin >> y;
+		cin >> z;
+	    if(w == (x+y+z) || w == (x+y) || w == (y+z) || w == (x+z) || w == x || w == y || w==z){
+	        cout << "YES" << "\n";
+	    }
+	    else{
+	        cout << "NO" << "\n";
+	    }
+	}
+}
